ec85975131279633f3802db7fb7aa52032756f56
hz-xiaxz <1806656034@qq.com>
2024-09-19 03:49:06 +0800

    revert N para commit

--- scripts/job.jl
+++ scripts/job.jl
@@ -7,25 +7,25 @@ using Dates
 
 tm = TaskMaker()
 tm.thermalization = 0
-tm.sweeps = 100000
-tm.binsize = 100
+tm.sweeps = 100
+tm.binsize = 1
 tm.n1 = 8
 tm.n2 = 8
 ns = tm.n1 * tm.n2 * 3
 tm.PBC = (true, false)
 tm.χ = 1.0
-for i = 1:10
-    task(tm; N_up = i, N_down = i)
+for i = ns÷2:-1:2
+    task(tm; N_up = i, N_down = ns - i)
 end
 
 dir = @__DIR__
 # savepath = dir * "/../data/" * Dates.format(Dates.now(), "mm-ddTHH-MM-SS")
-savepath = dir * "/../data/" * "$(tm.n1)x$(tm.n1)up=$(tm.N_up)down=$(tm.N_down)"
+savepath = dir * "/../data/" * "$(tm.n1)x$(tm.n1)"
 job = JobInfo(
     savepath,
     KagomeDSL.MC;
     tasks = make_tasks(tm),
-    checkpoint_time = "5:00",
+    checkpoint_time = "30:00",
     run_time = "24:00:00",
 )
 
