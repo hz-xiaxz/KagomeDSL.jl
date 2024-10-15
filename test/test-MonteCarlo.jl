1167daf9bf00f954b9baacddfdb5b956d1624a8d
LeoXia <114814844+hz-xiaxz@users.noreply.github.com>
2024-10-14 17:11:14 -0700

    update BestieTemplate (#21)
    * update BestieTemplate
    
    * make lint happy
    
    * make test happy
    
    * make lint happy
    
    * lint happy

--- test/test-MonteCarlo.jl
+++ test/test-MonteCarlo.jl
@@ -2,13 +2,7 @@ using KagomeDSL
 using Test
 
 @testset "MC" begin
-    param = Dict(
-        :n1 => 4,
-        :n2 => 3,
-        :PBC => (true, false),
-        :N_up => 18,
-        :N_down => 18,
-    )
+    param = Dict(:n1 => 4, :n2 => 3, :PBC => (true, false), :N_up => 18, :N_down => 18)
     test_mc = MC(param)
     @test sum(test_mc.conf_up) == 18
     @test sum(test_mc.conf_down) == 18
