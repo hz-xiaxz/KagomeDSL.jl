232b9c85d20280937d8674f3323d24193a5b669e
hzxiaxz <1806656034@qq.com>
2025-10-27 16:12:02 +0800

    Revert "right LL"
    This reverts commit e308cfaa3fe9e9c53752f6e1207d8e08e5b6901e.

--- scripts/LL.jl
+++ scripts/LL.jl
@@ -8,21 +8,21 @@ using Dates
 using LinearAlgebra
 
 tm = TaskMaker()
-tm.thermalization = 1000
-tm.sweeps = 5_000
+tm.thermalization = 5000
+tm.sweeps = 500_000
 tm.binsize = 2
-tm.n1 = 8 
-tm.n2 = 8 
+tm.n1 = 8
+tm.n2 = 8
 ns = tm.n1 * tm.n2 * 3
 tm.PBC = (true, true)
 tm.antiPBC = (false, true)
 tm.lattice = DoubleKagome
-imbalances = [0, 8, 16, 32, 40, 56]
+imbalances = [0, 2, 4, 8, 10, 14]
 for imbalance in imbalances
     S = tm.n1 * tm.n2 * 2√3
     tm.B = imbalance * π / S
     tm.imbalance = imbalance
-    task(tm; N_up = ns ÷ 2 , N_down = ns ÷ 2 )
+    task(tm; N_up = ns ÷ 2 + imbalance ÷ 2, N_down = ns ÷ 2 - imbalance ÷ 2)
 end
 
 dir = @__DIR__
